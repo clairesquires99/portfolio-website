ed88284ad39a4cc237f2ced582df7c15d8001fb4
Claire Squires <clairesquires99@outlook.com>
2026-04-18 22:24:05 -0400

    chore: add github actions to deploy to pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/portfolio-website/",
   plugins: [react(), tailwindcss()],
-})
+});
